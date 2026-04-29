3b74dbefd913a47e31ca9cddcebeffc9a9733340
martyn6000 <martynstanton@hotmail.com>
2026-04-29 09:26:18 +0800

    Signed-off-by: martyn6000 <martynstanton@hotmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='netbox-contracts',
-    version='1.5',
+    version='1.6',
     description='Custom plugin for Contract management',
     install_requires=[],
     packages=find_packages(),
